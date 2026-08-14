ff2c76a0184ff40a8c7b760d6c92da003dfadb30
Masukulmiguel <Masukulmiguel@users.noreply.github.com>
2026-08-14 14:50:53 +0100

    feat: apply Jack 3D Creator portfolio design, clean up unused files

--- src/components/three/index.ts
+++
@@ -1,6 +0,0 @@
-"use client";
-
-export { Scene } from "./Scene";
-export { FloatingObjects } from "./FloatingObjects";
-export { HeroModel } from "./HeroModel";
-export { CustomEnvironment } from "./Environment";
